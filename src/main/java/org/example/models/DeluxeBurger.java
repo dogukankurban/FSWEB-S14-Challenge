55e871dea792db0c6b1504ab806d7f2641966fd3
dogukankurban <c.d.kurban007@gmail.com>
2024-09-30 18:15:32 +0300

    challenge2

---
+++ src/main/java/org/example/models/DeluxeBurger.java
@@ -0,0 +1,58 @@
+package org.example.models;
+
+public class DeluxeBurger extends Hamburger{
+
+    private String cips;
+    private String drink;
+
+    public DeluxeBurger() {
+        super.setPrice(19.1);
+        this.cips = "CURVY";
+        this.drink = "COKE";
+
+    }
+
+    public String getCips() {
+        return cips;
+    }
+
+    public String getDrink() {
+        return drink;
+    }
+
+
+    public DeluxeBurger(String name, String meat,  String breadRollType) {
+        super(name, meat, 0.0, breadRollType);
+        this.cips = "CURVY";
+        this.drink = "COKE";
+    }
+
+    @Override
+    public void addHamburgerAddition1(String addition1Name, double addition1Price) {
+        System.out.println("Burger için yeni malzeme eklenemez.");
+    }
+
+    @Override
+    public void addHamburgerAddition2(String addition2Name, double addition2Price) {
+        System.out.println("Burger için yeni malzeme eklenemez.");
+    }
+
+    @Override
+    public void addHamburgerAddition3(String addition3Name, double addition3Price) {
+        System.out.println("Burger için yeni malzeme eklenemez.");
+    }
+
+    @Override
+    public void addHamburgerAddition4(String addition4Name, double addition4Price) {
+        System.out.println("Burger için yeni malzeme eklenemez.");
+    }
+
+
+
+    @Override
+    public String itemizeHamburger() {
+        double price = getPrice();
+        return "Name: " + getName() + " Meat: " + getMeat() + " BreadRollType: " + getBreadRollType() + " Price: " + price;
+    }
+
+}
---
+++ src/main/java/org/example/models/HealthyBurger.java
@@ -0,0 +1,49 @@
+package org.example.models;
+
+public class HealthyBurger extends Hamburger {
+    private String healthyExtra1Name;
+    private double healthyExtra1Price;
+
+    private String healthyExtra2Name;
+    private double healthyExtra2Price;
+
+    private String healthyExtra3Name;
+    private double healthyExtra3Price;
+
+    private String healthyExtra4Name;
+    private double healthyExtra4Price;
+
+    public HealthyBurger(){
+
+    }
+
+    public HealthyBurger(String name, double price, String breadRollType) {
+        super(name, "Tofu", price, breadRollType);
+    }
+
+
+    public void addHealthyAddition1(String ingredient , double price){
+        this.healthyExtra1Name=ingredient;
+        this.healthyExtra1Price=price;
+        setPrice(getPrice()+price);
+    }
+
+    public void addHealthyAddition2(String ingredient , double price){
+
+        this.healthyExtra2Name=ingredient;
+        this.healthyExtra2Price=price;
+        setPrice(getPrice()+price);
+    }
+
+
+    @Override
+    public String itemizeHamburger() {
+        double price = getPrice() + healthyExtra1Price + healthyExtra2Price;
+        return "Name : " + getName() + " " + "Meat:" + getMeat() +
+                "BreadRollType:" + getBreadRollType() + " " + "Addition:1" + healthyExtra1Name
+                + "Addition:2" + healthyExtra2Name +
+
+                "Price:" + " " + getPrice();
+    }
+}
+
